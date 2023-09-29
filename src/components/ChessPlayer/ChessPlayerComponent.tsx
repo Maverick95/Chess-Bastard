40d694efd1a9965c05c688def916448b2cb68adf
Nick Emmerson <nickemmerson619@hotmail.com>
2023-09-29 12:13:15 +0100

    Begin laying out last game card

--- src/components/ChessPlayer/ChessPlayerComponent.tsx
+++ src/components/ChessPlayer/ChessPlayerComponent.tsx
@@ -38,7 +38,7 @@ const ChessPlayerComponent: React.FC<IProps> = ({ username, lastLiveGameSeconds 
             refetchOnWindowFocus: false
         });
 
-    const { data: dataLastLiveGame } = useQuery(
+    const { data: lastLiveGame } = useQuery(
         `query-chess-last-live-game-${username}-${lastLiveGameSeconds}`,
         () => GetLastLiveGameService(username, lastLiveGameSeconds),
         {
@@ -52,13 +52,13 @@ const ChessPlayerComponent: React.FC<IProps> = ({ username, lastLiveGameSeconds 
     const player = useMemo(() => {
         if (dataPlayer) {
             const player_new = { ...dataPlayer };
-            if (dataLastLiveGame) {
-                player_new.lastLiveGame = dataLastLiveGame;
+            if (lastLiveGame) {
+                player_new.lastLiveGame = lastLiveGame;
             }
             return player_new;
         }
         return null;
-    }, [dataPlayer, dataLastLiveGame]);
+    }, [dataPlayer, lastLiveGame]);
 
     const sectionClassNamePlayerPart1 = isSuccessPlayer ? 'chess-player-card' : 'chess-player-card chess-centered';
     const sectionClassNamePlayerPart2 = details === ChessPlayerDisplayType.DISPLAY_PLAYER ? 'chess-player-card-active' : 'chess-player-card-last-active';
@@ -76,9 +76,10 @@ const ChessPlayerComponent: React.FC<IProps> = ({ username, lastLiveGameSeconds 
                     {isErrorPlayer && <ErrorComponent username={username} />}
                     {isSuccessPlayer && <SuccessComponent {...{ player, changeDetails }} />}
                 </section>
+                {lastLiveGame &&
                 <section id={`chess-last-live-game-card-${username}`} className={sectionClassNameLastLiveGame}>
-                    <LastLiveGameCardComponent {...{ changeDetails }} />
-                </section>
+                    <LastLiveGameCardComponent {...{ lastLiveGame, changeDetails }} />
+                </section>}
             </div>
         </div>
     );
