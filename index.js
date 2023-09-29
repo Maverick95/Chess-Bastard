c0a294042f6b88a79dd028d436cc33b73aaea8fe
Nick Emmerson <nickemmerson619@hotmail.com>
2023-09-29 12:11:39 +0100

    Trying to get cards to inline but weird bug

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ChessPlayerComponent from './src/components/ChessPlayer/ChessPlayerComponent';
+import ChessPanelComponent from './src/components/ChessPanel/ChessPanelComponent';
 
 const root = document.getElementById('root');
 
-ReactDOM.render((
-<>
-    <div style={{display: 'flex'}}>
-        <ChessPlayerComponent username="cheezburgers" />
-        <ChessPlayerComponent username="jamescookpandp" />
-        <ChessPlayerComponent username="nickemmerson" />
-        <ChessPlayerComponent username="pasp86" />
-        <ChessPlayerComponent username="lapsedpacifist" />
-    </div>
-</>
-) ,root);
+ReactDOM.render(<ChessPanelComponent />, root);
