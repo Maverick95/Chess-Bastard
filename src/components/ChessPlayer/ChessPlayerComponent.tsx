4860ea733c6e43f80ecfbfdd2d834b7bbc2c1aa7
Nick Emmerson <nickemmerson619@hotmail.com>
2023-09-29 12:11:34 +0100

    Include loading and error screens

--- src/components/ChessPlayer/ChessPlayerComponent.tsx
+++ src/components/ChessPlayer/ChessPlayerComponent.tsx
@@ -13,47 +13,70 @@ interface IState {
     player?: Player
 }
 
-const ChessPlayerComponent: React.FC<IProps> = ({username}) => {
+const LoadingComponent: React.FC = () => (
+    <section className="chess-player-card chess-centered">
+        <div className="chess-player-avatar">
+            <img className="chess-player-loading" {...{ width: 200, height: 200, src: './assets/loading.png', alt: 'Loading...' }} />
+        </div>
+    </section>
+);
 
-    const [state, setState] = useState<IState>({status: 'loading'});
+
+const ErrorComponent: React.FC<IProps>= ({username}) => (
+    <section className="chess-player-card chess-centered">
+        <div>error loading</div>
+        <div className="chess-detail-standout">{username}</div>
+    </section>
+);
+
+const SuccessComponent: React.FC<Player> = ({ username, avatar, lastOnline }) => {
+
+    avatar = avatar ?? "./assets/default_player.jpg";
+    const { difference } = getDateTimeDifferenceDescription(lastOnline);
+
+    return (
+    <section className="chess-player-card">
+        <header className="chess-player-header chess-border-theme">
+            {username}
+        </header>
+        <div className="chess-player-avatar">
+            <img className="chess-border-theme" {...{ width: 200, height: 200, src: avatar, alt: `chess.com avatar for ${username}` }} />
+        </div>
+        <div className="chess-player-content">
+            <div>last seen</div>
+            <div className="chess-detail-standout">{difference}</div>
+        </div>
+    </section>
+    );
+
+};
+
+const ChessPlayerComponent: React.FC<IProps> = ({ username }) => {
+
+    const [state, setState] = useState<IState>({ status: 'loading' });
 
     useEffect(() => {
-        setState({status: 'loading'});
-        const getData = async() => {
+        setState({ status: 'loading' });
+        const getData = async () => {
             try {
                 const player = await GetPlayerService(username);
-                setState({status: 'success', player});
+                setState({ status: 'success', player });
             }
             catch {
-                setState({status: 'error'});
+                setState({ status: 'error' });
             }
         };
         getData();
     }, [username]);
 
-    if (state.status === 'success') {
-        
-        const srcDefault = "./assets/default_player.jpg";
-        const src = state.player.avatar ?? srcDefault;
-        const lastOnlineDisplay = getDateTimeDifferenceDescription(state.player.lastOnline);
-
-        return (
-            <section className="chess-player-card">
-                <header className="chess-player-header chess-border-theme">
-                    {username}
-                </header>
-                <div className="chess-player-avatar">
-                    <img className="chess-border-theme" {...{ width: 200, height: 200, src, alt: `Avatar for ${username}`}} />
-                </div>
-                <div className="chess-player-content">
-                    <div>last seen</div>
-                    <div className="chess-player-last-seen">{lastOnlineDisplay.difference}</div>
-                </div>
-            </section>
-        );
+    if (state.status === 'loading') {
+        return <LoadingComponent />;
+    }
+    if (state.status === 'error') {
+        return <ErrorComponent username={username} />;
     }
 
-    return <p>Hello!</p>
+    return <SuccessComponent {...state.player} />;
 };
 
 export default ChessPlayerComponent;
